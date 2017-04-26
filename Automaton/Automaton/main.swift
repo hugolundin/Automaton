b1360dac1b2f194efddaa8faefc21a8896cf9081
Hugo Lundin <hugo@hugolundin.se>
2017-04-26 13:07:06 +0200

    Add initial layout of DFA struct

--- Automaton/Automaton/main.swift
+++ Automaton/Automaton/main.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+struct State {
+    let label: String
+    
+    init(_ label: String) {
+        self.label = label
+    }
+}
+
+protocol Automaton {
+    var states: [State]      { get set }
+    var symbols: [String]    { get set }
+    var initial: State       { get set }
+    var final: [State]       { get set }
+}
+
+struct DFA: Automaton {
+    var states: [State]
+    var symbols: [String]
+    var transition: (State, ([String]) -> State)
+    var initial: State
+    var final: [State]
+    
+    init(states: [State], symbols: [String], transition: (State, ([String]) -> State), initial: State, final: [State]) {
+        self.states     = states
+        self.symbols    = symbols
+        self.transition = transition
+        self.initial    = initial
+        self.final      = final
+    }
+}
+
+let dfa = DFA(states: [State("q1"), State("q2")], symbols: ["0", "1"], transition: nil, initial: State("q1"), final: [State("q2")])
+
+
+
